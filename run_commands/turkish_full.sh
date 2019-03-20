8e34f62f718bfd8dcb6a4dfb5268b8ff2497a697
akashrajkn <akashrajkn@gmail.com>
2019-03-20 19:32:10 +0100

    Modifications to get the code running 1. Added ntm_minibatch file. 2. Corrected number of arguments for preprocess function output. 3. Updated THEANO_FLAGS for the latest release. 4. Updated .gitignore to untrack obj files

--- run_commands/turkish_full.sh
+++ run_commands/turkish_full.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-THEANO_FLAGS="device=gpu,floatX=float32" python ../semi_models/run.py -bidirectional -worddrop 0.4 -lang turkish -kl_thres 0.2 -start_val 15000 -epochs 120 -add_uns 0.0
+THEANO_FLAGS="floatX=float32,optimizer_excluding=scanOp_pushout_output" python ../semi_models/run.py -bidirectional -worddrop 0.4 -lang turkish -kl_thres 0.2 -start_val 15000 -epochs 120 -add_uns 0.0
